5f9c7f386da0d5667d633bc7863ebd6875f3cdc1
Diocris <diocris.r@gmail.com>
2023-07-28 11:27:54 +0200

    updated

---
+++ src/models/PostsDatabase.ts
@@ -0,0 +1,5 @@
+import { BaseDatabase } from "./BaseDatabase";
+
+export class PostsDatabase {
+    public static POSTS_TABLE = "posts"
+}
---
+++ src/models/UserDatabase.ts
@@ -0,0 +1,6 @@
+import { BaseDatabase } from "./BaseDatabase";
+
+export class UserDatabase {
+    public static USERS_TABLE = "users"
+
+}
